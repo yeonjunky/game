7a006980268632c3a7f584f20665f34f3fa0a7eb
yeonjunky <yeonjunky@gmail.com>
2020-03-07 00:09:28 +0900

    flappy_bird.py

--- venv/src/flappy_bird.py
+++ venv/src/flappy_bird.py
@@ -1,5 +1,6 @@
 import pygame
 from pygame.locals import *
+import sys
 import random
 
 pygame.init()
@@ -7,11 +8,6 @@ pygame.init()
 blist=[[50,310],[60,300],[70,290],[80,280],[90,270],[100,260],[110,250],[120,240],[130,230],[140,220],[150,210],[160,200],[170,190],[180,180],
        [190,170],[200,160],[210,150],[220,140],[230,130],[240,120],[250,110],[260,100],[270,90],[280,80]
        ,[290,70],[300,60],[310,50]]
-black = (255,255,255)
-white = (0, 0, 0)
-red = (255, 0, 0)
-green = (0, 255, 0)
-blue = (0, 0, 255)
 skyblue = (135, 206, 235)
 
 WIN_WIDHT = 600
@@ -42,7 +38,6 @@ class Player:
     def draw(self, window):
         window.blit(self.img, (self.x, self.y))
 
-
     def get_mask(self):
         return pygame.mask.from_surface(self.img)
 
@@ -63,12 +58,18 @@ class Pipe:
     def move(self):
         self.x -= self.vel
 
-    def display(self, win):
-        win.bilt(self.top_image, (self.x, self.top))
-        win.bilt(self.bottom_image, (self.x, self.bottom))
+    def display(self, window):
+        window.bilt(self.top_image, (self.x, self.top))
+        window.bilt(self.bottom_image, (self.x, self.bottom))
 
     def collide(self):
         pygame.sprite.collide_mask()
+
+def game_window(screen, bird, pipe, score):
+    pass
+
+def draw_text(text):
+    return pygame.font.Font.render('arial', "Score: " + str(text), 1, (255, 255, 255))
 
 def main():
     score = 0
@@ -76,16 +77,21 @@ def main():
     pipe = [Pipe(700)]
 
     while True:
-        for evnet in pygame.event.get():
-            if event.type == pygame.quit():
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
                 pygame.quit()
+                sys.exit()
                 break
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_SPACE:
                     game_start = True
                     bird.jump()
 
-    
+
+    pygame.display.flip()
+
+
+
 
 if __name__ == "__main__":
     main()
